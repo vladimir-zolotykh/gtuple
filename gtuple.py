681672ee67b0f377a7d1fc1b1293bc39acb7bde3
Vladimir Zolotykh <vladimir.zolotykh@gmail.com>
2026-05-07 09:47:38 +0300

    Initial import

---
+++ gtuple.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+# PYTHON_ARGCOMPLETE_OK
+
+
+class TupleMeta(type):
+    pass
+
+
+class Tuple(metaclass=TupleMeta):
+    def __new__(cls, *args):
+        return super().__new__(cls, args)
+
+
+if __name__ == "__main__":
+    tup = Tuple(("a", "b"))
+    print(tup)

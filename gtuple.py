9f57815c06058830fbd5801edd84479936766ff2
Vladimir Zolotykh <vladimir.zolotykh@gmail.com>
2026-05-07 10:02:19 +0300

    Add test_gtuple() [pytest]

--- gtuple.py
+++ gtuple.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 # PYTHON_ARGCOMPLETE_OK
+import pytest
 
 
 class TupleMeta(type):
@@ -9,9 +10,22 @@ class TupleMeta(type):
 
 class Tuple(tuple, metaclass=TupleMeta):
     def __new__(cls, *args):
+        if len(args) != 2:
+            raise ValueError("Tuple takes 2 arguments")
         return super().__new__(cls, args)
 
 
+def test_gtuple():
+    tup = Tuple("a", "b")
+    assert tup == ("a", "b")
+    assert tup[0] == "a"
+    assert tup[1] == "b"
+    with pytest.raises(AttributeError):
+        tup.x
+    with pytest.raises(ValueError):
+        tup = Tuple(1, 2, 3)
+
+
 if __name__ == "__main__":
-    tup = Tuple(("a", "b"))
+    tup = Tuple("a", "b")
     print(tup)
